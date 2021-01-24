eb8be133461ef9a84ba4a4f66a4940e0493bd4cb
Ilija Krsteski <krsteski@Ilijas-MacBook-Pro.fritz.box>
2021-01-24 22:16:49 +0100

    starting files with firebase configuration done

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB6liEYxa6wlv20tBO9wDxa20YpOkwKaeA",
+  authDomain: "clone-f8cb2.firebaseapp.com",
+  projectId: "clone-f8cb2",
+  storageBucket: "clone-f8cb2.appspot.com",
+  messagingSenderId: "885876781197",
+  appId: "1:885876781197:web:8ca829c3b8866fd53eb2c4"
+};
+
